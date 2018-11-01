45031a549347b7e0947ec991f7bc625a9a216c17
deepak1418 <deepakjd44@yahoo.com>
2018-11-01 14:57:55 +0530

    first commit

---
+++ credentials.js
@@ -0,0 +1,28 @@
+
+// Datanautix help bot
+//var accessToken = "b56ec2c85b2744ad81aeb6518d30a6ae";
+
+    //----------------------- Emoji Bot ----------------------- //
+    var credentialsAccessToken ="fe3563c6ac5f43be9714f35d7572e692";
+
+    //var bot name is used for the firebase database
+    var credentialsBotName = "buddy-d9942";
+
+
+    // // HighSchoolAna(Dev)
+    // var credentialsAccessToken = "5489544adf6d490c8438cb7377f4bd60"
+
+    // var credentialsBotName = "High School Ana (Dev)"
+
+
+    var credentialsBaseUrl = "https://api.api.ai/v1/";
+
+    // Initialize Firebase
+    var credentialsConfig = {
+       apiKey: "AIzaSyC5du1J31Z9pB-_YaVOhmgmhpcupxcHrN8",
+       authDomain: "buddy-d9942.firebaseapp.com",
+       databaseURL: "https://buddy-d9942.firebaseio.com",
+       projectId: "buddy-d9942",
+       storageBucket: "buddy-d9942.appspot.com",
+       messagingSenderId: "1082877982683"
+    };
